7b8c17dfd14b42a80136cca2c809c21ab4172971
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2022-10-10 17:49:28 -0400

    EH-20 setup the dev backend work

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Edis-Hub
+          Welcome to Edis-Hub!
         </h1>
 
         <p className={styles.description}>
