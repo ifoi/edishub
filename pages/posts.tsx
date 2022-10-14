e6d1f57cffd90f5fb0bb3b996eca1ebe103ed018
Ifo Ikede <ifoitech@gmail.com>
2022-10-14 15:35:30 -0300

    faustjs and gql initial configuration

---
+++ pages/posts.tsx
@@ -0,0 +1,17 @@
+import { client } from 'client';
+import Post from 'components/post'
+
+
+export default function PostsPage( ) {
+  const { usePosts } = client;
+  const posts = usePosts()?.nodes ;
+
+  return (
+    <div>
+      <h1> My posts page  </h1>   
+      {posts.map( post => (
+          <Post key={post.id} post={post} />
+      ))}
+    </div> 
+    );
+}
