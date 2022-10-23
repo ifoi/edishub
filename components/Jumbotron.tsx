9b9e70c8c9e77b8bb98d6400b3390582bb49948e
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2022-10-23 09:47:57 -0400

    Style the home page jumbotron

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -1,18 +1,30 @@
+import Image from "next/image"
 import Link from "next/link"
+import AmicoImg from '../public/amico.png'
+import styles from '../styles/Jumbotron.module.scss'
 
-interface Props {}
+interface Props { }
 
 function Jumbotron(props: Props) {
-  const {} = props
+  const { } = props
 
   return (
-    <div className="p-5 mb-4 bg-light rounded-3">
-      <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">Your <span>Hub</span> to Tech Resources</h1>
-        <p className="col-md-8 fs-4">Catchy phrase placeholder</p>
-        <Link href="/posts">
-          <a className="btn btn-primary btn-lg" type="button" rel="norefferer noopener">Explore Resources</a>
-        </Link>
+    <div className="p-5 mb-4 theme-bg">
+      <div className="container d-flex justify-content-between">
+        <div className="left-side" style={{ width: '50%' }}>
+          <h1 className="display-5 fw-bold">Your <span className="special-text">Hub</span> to</h1>
+          <h1 className="display-5 fw-bold">Tech Resources</h1>
+          <p className="fs-4">The internet is an ocean full of resources. Let’s help you cast your net and find what’s right for you.</p>
+          <Link href="/posts">
+            <a className="btn blue-btn btn-lg" type="button" rel="norefferer noopener">Explore Resources</a>
+          </Link>
+          <Link href="/posts">
+            <a className="btn white-btn btn-lg mx-4" type="button" rel="norefferer noopener">View Dashboard</a>
+          </Link>
+        </div>
+        <div className={styles.imageFormat}>
+          <Image src={AmicoImg} alt="amico" />
+        </div>
       </div>
     </div>
   )
