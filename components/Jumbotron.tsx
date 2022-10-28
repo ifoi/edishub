683375a11adcfbd0256ea640b0d4236ac71d0597
Ifo Ikede <ifoitech@gmail.com>
2022-10-27 23:41:05 -0300

    first stab at signup POPup functionality

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -3,10 +3,42 @@ import Link from "next/link"
 import AmicoImg from '../public/amico.png'
 import styles from '../styles/Jumbotron.module.scss'
 
+import{ Overlay, ClickButton, PopupCard} from "styles/Popup" ;
+
+import {useState} from "next/useState" ;
+import { MouseEvent } from "react";
+
 interface Props { }
+
+// type Popup = {
+//   togglePopup: (event: React.MouseEvent<HTMLElement>) => void;
+// }
+
+// const Popup = ({ togglePopup } : Popup) => (
+//       <Overlay>
+//       <PopupCard>
+//         <ClickButton onClick={togglePopup}>Close</ClickButton>
+//       </PopupCard>
+//     </Overlay>
+// );
+
+
+
 
 function Jumbotron(props: Props) {
   const { } = props
+
+  type Popup = {
+    togglePopup: (event: React.MouseEvent<HTMLElement>) => void;
+  }
+  
+  const Popup = ({ togglePopup } : Popup) => (
+        <Overlay>
+        <PopupCard>
+          <ClickButton onClick={togglePopup}>Close</ClickButton>
+        </PopupCard>
+      </Overlay>
+  );
 
   return (
     <div className="p-5 mb-4 theme-bg">
@@ -21,6 +53,10 @@ function Jumbotron(props: Props) {
           <Link href="/temp_resources">
             <a className="btn white-btn btn-lg mx-4" type="button" rel="norefferer noopener">View Dashboard</a>
           </Link>
+         {/* <Popup togglePopup={function (event: MouseEvent<HTMLElement, MouseEvent>): void {
+            throw new Error("Function not implemented.");  
+                  } } />
+          */} 
         </div>
         <div className={styles.imageFormat}>
           <Image src={AmicoImg} alt="amico" />
