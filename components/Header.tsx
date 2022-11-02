9fe980f5b6f7f391be07d9c5a2cd8e754057eb2a
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2022-11-01 22:06:53 -0400

    Add the right file extension to some files and add the Login component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,55 +1,59 @@
 import Link from "next/link"
 import styled from "styled-components"
 
-import {EdisHub,Group14, Categories, Group27, Rectangle16, SearchForAnything, Vector} from "./styles"
+import { EdisHub, Group14, Categories, Group27, Rectangle16, SearchForAnything, Vector } from "./styles"
 
 
-interface Props {}
+interface Props { }
 
 function Header(props: Props) {
-  const {} = props
+  const { } = props
 
   return (
-      <div className="container">
-    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      
-      <EdisHub >
-      <Link href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-           <h2>Edis Hub</h2>
-        </Link> 
-       
-       </EdisHub >
-         {/* <EdisHub>
-          <Link href="/">  EdisHub   
-          </Link>
-          
-          </EdisHub> */}
-       
-      
-     
-      <Group14>
-        <Categories>Categories</Categories>
-      </Group14>
-   
-      <Group27>
-        <Rectangle16 />
-        <SearchForAnything>Search for Anything</SearchForAnything>
-      </Group27>
-      
-      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
-        {/* <li><a href="#" className="nav-link px-2 link-dark">Features</a></li>
-        <li><a href="#" className="nav-link px-2 link-dark">Pricing</a></li> */}
-        <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
-        <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
-      </ul>
+    <div className="container">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
 
-      <div className="col-md-3 text-end">
-        <button type="button" className="btn btn-outline-primary me-2">Login</button>
-        <button type="button" className="btn btn-primary">Sign-up</button>
-      </div>
-    </header>
-  </div>
+        <EdisHub >
+          <Link href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <h2>Edis Hub</h2>
+          </Link>
+
+        </EdisHub >
+        {/* <EdisHub>
+          <Link href="/">  EdisHub
+          </Link>
+
+          </EdisHub> */}
+
+
+
+        <Group14>
+          <Categories>Categories</Categories>
+        </Group14>
+
+        <Group27>
+          <Rectangle16 />
+          <SearchForAnything>Search for Anything</SearchForAnything>
+        </Group27>
+
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
+          {/* <li><a href="#" className="nav-link px-2 link-dark">Features</a></li>
+        <li><a href="#" className="nav-link px-2 link-dark">Pricing</a></li> */}
+          <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
+          <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
+        </ul>
+
+        <div className="col-md-3 text-end">
+          <Link href="/login">
+            <a className="btn btn-outline-primary me-2">
+              Login
+            </a>
+          </Link>
+          <button type="button" className="btn btn-primary">Sign-up</button>
+        </div>
+      </header>
+    </div>
   )
 }
 
---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignupPopup from '../components/SignupPopup'
+
+interface Props { }
+
+function Login(props: Props) {
+  const { } = props
+
+  return (
+    <SignupPopup />
+  )
+}
+
+export default Login
