23cf4659d13f03bd78494cd3c6e39d859ca3acb7
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2022-11-03 20:47:49 -0400

    Add the markup for the Usercard component

---
+++ pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import Usercard from 'components/Usercard'
+import React from 'react'
+import Header from '../components/Header'
+interface Props { }
+
+function Dashboard(props: Props) {
+  const { } = props
+
+  return (
+    <>
+      <Usercard />
+    </>
+  )
+}
+
+export default Dashboard
+
