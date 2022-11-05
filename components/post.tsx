166bcfb8cd7212edb6f6c02fc20807916134c884
Ifo Ikede <ifoitech@gmail.com>
2022-11-05 20:25:21 -0300

    reenabled post and posts.tsx

--- components/post.tsx
+++ components/post.tsx
@@ -1,21 +1,22 @@
 // import { Post as PostType } from 'client' ;
 
-// export interface PostProps {
-//     post: PostType;
-//   }
+export interface PostProps {
+    // post: PostType;
+    post: any;
+  }
   
-//   export default function Post(props: PostProps) {
-//     const { post } = props;
+  export default function Post(props: PostProps) {
+    const { post } = props;
   
-//     return (
-//       <article>
-//         <h1>{post.title()}</h1>
-//         <div dangerouslySetInnerHTML= {{ __html: post.content() }} />
-//       </article>
-//     );
-//   }
+    return (
+      <article>
+        <h1>{post.title()}</h1>
+        <div dangerouslySetInnerHTML= {{ __html: post.content() }} />
+      </article>
+    );
+  }
 
-export {}
+
 
 
 
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,21 +1,19 @@
-// import { client } from 'client';
-// import Post from 'components/post'
+import { client } from 'client';
+import Post from 'components/post'
 
 
-// export default function PostsPage( ) {
-//   const { usePosts } = client;
-//   const posts = usePosts()?.nodes ;
+export default function PostsPage( ) {
+  const { usePosts } = client;
+  const posts = usePosts()?.nodes ;
 
-//   return (
-//     <div>
-//       <h1> My posts page  </h1>
-//       {posts.map( post => (
-//         <div key={post.id}>
-//           <Post  post={post} />
-//         </div>
-//       ))}
-//     </div>
-//     );
-// }
-
-export {}
+  return (
+    <div>
+      <h1> My posts page  </h1>
+      {posts.map( post => (
+        <div key={post.id}>
+          <Post  post={post} />
+        </div>
+      ))}
+    </div>
+    );
+}
