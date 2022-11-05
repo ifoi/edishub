d797d1eb7f04a2d7c054046bdb31f4f850e87f11
Ifo Ikede <ifoitech@gmail.com>
2022-11-05 19:59:04 -0300

    disable post and posts.tsx

--- components/post.tsx
+++ components/post.tsx
@@ -1,19 +1,21 @@
-import { Post as PostType } from 'client' ;
+// import { Post as PostType } from 'client' ;
 
-export interface PostProps {
-    post: PostType;
-  }
+// export interface PostProps {
+//     post: PostType;
+//   }
   
-  export default function Post(props: PostProps) {
-    const { post } = props;
+//   export default function Post(props: PostProps) {
+//     const { post } = props;
   
-    return (
-      <article>
-        <h1>{post.title()}</h1>
-        <div dangerouslySetInnerHTML= {{ __html: post.content() }} />
-      </article>
-    );
-  }
+//     return (
+//       <article>
+//         <h1>{post.title()}</h1>
+//         <div dangerouslySetInnerHTML= {{ __html: post.content() }} />
+//       </article>
+//     );
+//   }
+
+export {}
 
 
 
