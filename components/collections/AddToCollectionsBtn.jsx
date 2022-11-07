3885c12c53399e706e9ea6b50414687acb0b77b0
Ifo Ikede <ifoitech@gmail.com>
2022-11-06 21:05:29 -0400

    EH-16 update AddToCollections

--- components/collections/AddToCollectionsBtn.jsx
+++ components/collections/AddToCollectionsBtn.jsx
@@ -2,12 +2,18 @@ import Link from "next/link";
 import Button from "react-bootstrap/Button";
 
 
+const handleAddToCollectionsBtnClick = () => {
+
+    // if collection has items, update collection state
+
+    //else create new collection , and push item to collection
+}
 
 const AddToCollectionsBtn = (props) => {
 
     return (
         <>
-        <Button size="sm"> Add to Collections</Button>
+        <Button onClick={handleAddToCollectionsBtnClick} size="sm"> Add to Collections</Button>
         </>
     )
 }
