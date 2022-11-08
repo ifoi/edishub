fbb550f657ecfb08e91d15af7a19e52f2884a6e8
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2022-11-08 09:59:08 -0500

    Enable headers to pass along every other requests

--- graphql/mutations/user.mutations.ts
+++ graphql/mutations/user.mutations.ts
@@ -1,11 +1,14 @@
 import { gql } from '@apollo/client'
 
 export const LOGIN_USER = gql`
-  mutation LoginUser($login: String!, $password: String!) {
-    loginWithCookies(input: {login: $login, password: $password}) {
-      clientMutationId
-      status
+  mutation LoginUser($username: String!, $password: String!) {
+    login(input: {username: $username, password: $password}) {
+    authToken
+    user {
+      id
+      name
     }
+  }
 }
 `
 
